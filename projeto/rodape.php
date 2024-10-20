117b2f912f5de49e13502431489a1630a0dffb26
D.  K.  Mariano <144285758+DKMariano@users.noreply.github.com>
2024-10-19 21:42:40 -0300

    Delete projeto directory

--- projeto/rodape.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo "<footer>";
-    echo "<p>Acessado por " . $_SERVER['REMOTE_ADDR'] . " em " . date("d/m/Y") . "</p>";
-    echo "<p>Desenvolvido por Dimitrius Khouri &copy; 2024</p>";
-    echo "</footer>";
-    $banco->close();     
-?>
